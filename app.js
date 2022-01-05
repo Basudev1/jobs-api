a67a3217560e47d9f6b2078a579823dc432fc1f1
Basudev <murabasudev@gmail.com>
2022-01-05 07:59:51 +0530

    Added Basic Security layer to app

--- app.js
+++ app.js
@@ -14,9 +14,24 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+//security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 app.use(express.json());
 // extra packages
-
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.set("trust proxy", 1);
 // routes
 app.use("/api/auth", authRouter);
 app.use("/api/jobs", authenticateUser, jobsRouter);
